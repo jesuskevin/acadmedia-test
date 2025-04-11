96d479a77a3c39fa52d6352569186969cb062164
Kevin <de.jesuskevin01@hotmail.com>
2025-04-10 21:38:50 -0400

    Implement CRUDs and Auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,24 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\CommunicationController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\EnrollmentController;
+use App\Http\Controllers\PaymentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('register', [AuthController::class,'regsiter']);
+Route::post('login', [AuthController::class,'login']);
+Route::post('logout', [AuthController::class,'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('courses', CourseController::class)->middleware(['role_or_permission:manage courses']);
+    Route::apiResource('enrollments', EnrollmentController::class)->middleware(['role_or_permission:manage enrollemnts']);
+    Route::apiResource('payments',PaymentController::class)->middleware(['role_or_permission:manage payments']);
+    Route::apiResource('communications', CommunicationController::class)->middleware(['role_or_permission:manage communications']);
+});
